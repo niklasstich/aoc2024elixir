4de2a2b5046fc373f0d7ba04861d01d4183c421f
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2024-12-02 18:55:12 +0100

    Day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,4 +1,5 @@
 defmodule AdventOfCode.Day02 do
+  alias Utils.EnumUtils
   :asc
   :desc
   def parse_line(line) do
@@ -10,22 +11,38 @@ defmodule AdventOfCode.Day02 do
   end
 
   def line_safe?(line) do
-    line |> Enum.reduce({true, nil, nil}, &line_safe_acc/2) |> elem(0)
+    line |> Enum.with_index() |> Enum.reduce({true, nil, nil, []}, &line_safe_acc/2) |> elem(0)
   end
 
-  def line_safe_acc(n, {safe, previous, prevdir}) do
-      case {safe, previous, prevdir} do
-        {_, nil, _} -> {true, n, nil}
-        {false, _, _} -> {false, n, nil}
-        {true, _, _} ->
+  def line_safe_dampened?(line) do
+    lineidx = Enum.with_index(line)
+    pass1 = lineidx |> Enum.reduce({true, nil, nil, []}, &line_safe_acc/2)
+    if elem(pass1, 0) do
+      true
+    else
+      idxs = elem(pass1, 3) |> Enum.flat_map(& [&1, &1-1, 0])
+      idxs |> Enum.map(fn i -> line_safe_dampened_inner?(lineidx, i) end) |> Enum.any?(& &1)
+    end
+  end
+
+  defp line_safe_dampened_inner?(line, i) do
+    line |> EnumUtils.reject_index(i) |> Enum.reduce({true, nil, nil, []}, &line_safe_acc/2) |> elem(0)
+  end
+
+  def line_safe_acc({n, i}, {safe, previous, prevdir, failedon}) do
+      case {safe, previous, prevdir, failedon} do
+        {true, nil, nil, _} -> {true, n, nil, failedon}
+        #stop updating when we failed so we can get both previous and n we failed on
+        #{false, _, _, _} -> {false, previous, nil, failedon}
+        {_, _, _, _} ->
           d = previous - n
           absd = abs(d)
           dir = if d >= 0, do: :asc, else: :desc
 
           if absd < 1 || absd > 3 || (prevdir != nil && prevdir != dir) do
-            {false, n, nil}
+            {false, n, dir, [i | failedon]}
           else
-            {true, n, dir}
+            {safe, n, dir, failedon}
           end
       end
   end
@@ -34,6 +51,7 @@ defmodule AdventOfCode.Day02 do
     args |> parse_input() |> Enum.map(&line_safe?/1) |> Enum.count(& &1)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args |> parse_input() |> Enum.map(&line_safe_dampened?/1) |> Enum.count(& &1)
   end
 end
---
+++ lib/utils/enum_utils.ex
@@ -0,0 +1,11 @@
+defmodule Utils.EnumUtils do
+  def except_first([], _value), do: []
+  def except_first([value | tail], value), do: tail
+  def except_first([head | tail], value) do
+    [head | except_first(tail, value)]
+  end
+
+  def reject_index(enum, index) do
+    enum |> Enum.with_index() |> Enum.reject(fn {_, i} -> i == index end) |> Enum.map(& elem(&1, 0))
+  end
+end
