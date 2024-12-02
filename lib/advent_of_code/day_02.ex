1e6d406419784098eebfc6670a449ee8c3c8355b
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2024-12-02 07:06:26 +0100

    Day 2 Part 1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,37 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  :asc
+  :desc
+  def parse_line(line) do
+    line |> String.split(" ") |> Enum.map(&String.to_integer/1)
+  end
+
+  def parse_input(input) do
+    input |> String.split("\n") |> Enum.filter(& &1 != "") |> Enum.map(&parse_line/1)
+  end
+
+  def line_safe?(line) do
+    line |> Enum.reduce({true, nil, nil}, &line_safe_acc/2) |> elem(0)
+  end
+
+  def line_safe_acc(n, {safe, previous, prevdir}) do
+      case {safe, previous, prevdir} do
+        {_, nil, _} -> {true, n, nil}
+        {false, _, _} -> {false, n, nil}
+        {true, _, _} ->
+          d = previous - n
+          absd = abs(d)
+          dir = if d >= 0, do: :asc, else: :desc
+
+          if absd < 1 || absd > 3 || (prevdir != nil && prevdir != dir) do
+            {false, n, nil}
+          else
+            {true, n, dir}
+          end
+      end
+  end
+
+  def part1(args) do
+    args |> parse_input() |> Enum.map(&line_safe?/1) |> Enum.count(& &1)
   end
 
   def part2(_args) do
--- lib/mix/tasks/d02.p1.ex
+++ lib/mix/tasks/d02.p1.ex
@@ -2,10 +2,19 @@ defmodule Mix.Tasks.D02.P1 do
   use Mix.Task
 
   import AdventOfCode.Day02
+  import AdventOfCode.Input
 
   @shortdoc "Day 02 Part 1"
   def run(args) do
-    input = nil
+    input = get!(2)
+#     input = """
+# 7 6 4 2 1
+# 1 2 7 8 9
+# 9 7 6 2 1
+# 1 3 2 4 5
+# 8 6 4 4 1
+# 1 3 6 7 9
+# """
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
