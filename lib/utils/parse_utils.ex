a7411e9b79c384c8ad418a303648926ce10cf08b
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2024-12-06 17:18:17 +0100

    Fix utils

--- lib/utils/parse_utils.ex
+++ lib/utils/parse_utils.ex
@@ -1,16 +1,16 @@
 defmodule Utils.Parse do
 
-  defp pad_input(lines, padding) do
+  defp pad_input(lines, padding, padding_char) do
     # we only need the length of the first line
     [firstline | _] = lines
     len = String.length(firstline)
     # make enough padding dots
-    padline = 1..(len+2*padding) |> Enum.map(fn _ -> "." end) |> Enum.join()
+    padline = 1..(len+2*padding) |> Enum.map(fn _ -> padding_char end) |> Enum.join()
     padding_list = padline |> List.duplicate(padding)
-    padding_list ++ Enum.map(lines, & "#{String.duplicate(".", padding)}#{&1}#{String.duplicate(".", padding)}") ++ padding_list
+    padding_list ++ Enum.map(lines, & "#{String.duplicate(padding_char, padding)}#{&1}#{String.duplicate(padding_char, padding)}") ++ padding_list
   end
-  def to_padded_2dmap(lines, padding \\ 1) do
-    lines = pad_input(lines, padding)
+  def to_padded_2dmap(lines, padding \\ 1, padding_char \\ ".") do
+    lines = pad_input(lines, padding, padding_char)
     lines |> Enum.with_index() |> Enum.reduce(%{}, fn {line, linenum}, acc ->
       line_to_2dmap_values({line, linenum}, acc)
     end)
--- lib/utils/vector_utils.ex
+++ lib/utils/vector_utils.ex
@@ -6,8 +6,8 @@ defmodule Utils.Vector do
   def y_part({_,y}), do: y
 
   def rotate_90deg({x,y}), do: rotate({x,y}, :math.pi/2)
-  def rotate_90deg_ccw({x,y}), do: rotate({x,y}, -90)
-  def rotate({x,y}, deg), do: {:math.cos(deg*x)-:math.sin(deg*y), :math.sin(deg*x)+:math.cos(deg*y)}
+  def rotate_90deg_ccw({x,y}), do: rotate({x,y}, -:math.pi/2)
+  def rotate({x,y}, deg), do: {x*:math.cos(deg)-y*:math.sin(deg), x*:math.sin(deg)+y*:math.cos(deg)}
   def rotate_rounded({x,y}, deg) do
     {x,y} = rotate({x,y},deg)
     {round(x), round(y)}
