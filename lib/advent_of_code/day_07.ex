5546702a851be8f30e1fe3c701fde330fe679d77
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2024-12-14 05:46:58 +0100

    Day 7

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,52 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+
+  def parse(input) do
+    input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [head|tail] = String.split(line, ": ")
+        [rest|_] = tail
+        {String.to_integer(head), rest |> String.split(" ") |> Enum.map(& String.to_integer(&1))}
+      end)
   end
 
-  def part2(_args) do
+  def permutate(_chars, 0), do: [[]]
+  def permutate(chars, n) do
+    for c <- chars, rest <- permutate(chars, n - 1), do: [c | rest]
+  end
+
+  def calc_rpn(list, target) do
+    list
+    |> Enum.reduce_while([], fn
+      x, [a, b | rest] when x in ["+", "*", "||"] ->
+        result =
+          case x do
+            "+" -> b + a
+            "*" -> b * a
+            "||" -> String.to_integer("#{a}#{b}")
+          end
+        if a > target, do: {:halt, [result]}, else: {:cont, [result | rest]}
+      x, stack -> {:cont, [x | stack]}
+    end)
+    |> hd()
+  end
+
+  def part1(args) do
+    process(args, ["+", "*"])
+  end
+
+  def part2(args) do
+    process(args, ["+", "*", "||"])
+  end
+
+  defp process(args, symbols) do
+    args
+      |> parse()
+      |> Task.async_stream(fn {target, list} ->
+        permutations = permutate(symbols, Enum.count(list) - 1)
+        if permutations |> Enum.map(&calc_rpn((list |> Enum.reverse()) ++ &1, target)) |> Enum.any?(& &1 == target), do: target, else: 0
+      end)
+      |> Enum.map(fn {:ok, result} -> result end)
+      |> Enum.sum()
   end
 end
--- lib/mix/tasks/d07.p2.ex
+++ lib/mix/tasks/d07.p2.ex
@@ -2,10 +2,22 @@ defmodule Mix.Tasks.D07.P2 do
   use Mix.Task
 
   import AdventOfCode.Day07
+  import AdventOfCode.Input
 
   @shortdoc "Day 07 Part 2"
   def run(args) do
-    input = nil
+    input = get!(7)
+#     input = """
+# 190: 10 19
+# 3267: 81 40 27
+# 83: 17 5
+# 156: 15 6
+# 7290: 6 8 6 15
+# 161011: 16 10 13
+# 192: 17 8 14
+# 21037: 9 7 18 13
+# 292: 11 6 16 20
+# """
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
